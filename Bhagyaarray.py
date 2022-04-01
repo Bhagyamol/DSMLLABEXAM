fc9e98be92d77ccbaa75b487768d0d2d848839c7
Bhagyamol <74665675+Bhagyamol@users.noreply.github.com>
2022-04-01 11:51:24 +0530

    Add files via upload

---
+++ Bhagyaarray.py
@@ -0,0 +1,9 @@
+import numpy as np
+x=np.array([[2,4,6,8],[1,2,3,4],[4,5,6,7],[1,1,1,2]])
+print("ARRAY")
+print(x)
+print("Display all elements excluding first row")
+print(x[1:])
+print("Display 3rd and 4th element of 1st row")
+print(x[0,2:4])
+
